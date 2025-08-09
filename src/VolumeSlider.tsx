ecf8fc7d7d24beb1d76cfec750b6cd11e8161753
Marek Miklaszewski <mar.mik1@wp.pl>
2025-08-09 15:36:56 +0200

    feat: handle fullscreen, add volume slider and InfoBox

---
+++ src/VolumeSlider.tsx
@@ -0,0 +1,74 @@
+import { PointerEvent, useRef } from 'react';
+
+interface VolumeSliderProps {
+  volume: number;
+  isMuted: boolean;
+  updateVolume: (volume: number) => void;
+  setIsVolumeHover: (isVolumeHover: boolean) => void;
+  setIsAdjustingVolume: (isAdjustingVolume: boolean) => void;
+}
+
+export function VolumeSlider({
+  volume,
+  updateVolume,
+  setIsAdjustingVolume,
+  setIsVolumeHover,
+  isMuted,
+}: VolumeSliderProps) {
+  const sliderRef = useRef<HTMLDivElement>(null);
+
+  const handlePointerDown = (ev: PointerEvent<HTMLDivElement>) => {
+    const controller = new AbortController();
+    const { signal, abort } = controller;
+
+    const setNewTime = (ev: globalThis.PointerEvent | PointerEvent) => {
+      if (!sliderRef.current) return;
+
+      const parentBox = sliderRef.current.getBoundingClientRect();
+      const valueY = ev.clientY - parentBox.top;
+      const percent = Math.min(Math.max(valueY / parentBox.height, 0), 1);
+      const newVolume = 1 - percent;
+
+      updateVolume(newVolume);
+      setIsAdjustingVolume(true);
+    };
+    const handleUp = () => {
+      abort.call(controller);
+      setIsAdjustingVolume(false);
+      setTimeout(() => {
+        setIsVolumeHover(false);
+      }, 1000);
+    };
+    const handleDrag = (e: Event) => e.preventDefault();
+
+    const handleMove = (ev: globalThis.PointerEvent) => {
+      setNewTime(ev);
+    };
+
+    setNewTime(ev);
+    window.addEventListener('pointermove', handleMove, { signal });
+    window.addEventListener('dragstart', handleDrag, { signal });
+    window.addEventListener('pointerup', handleUp, { signal });
+  };
+
+  return (
+    <div
+      onPointerDown={handlePointerDown}
+      tabIndex={0}
+      role="slider"
+      aria-valuemin={0}
+      aria-valuemax={1}
+      aria-valuenow={volume}
+      aria-valuetext={`Volume set to${volume * 100}%`}
+      aria-label="Volume slider"
+      className="volume-slider-container"
+    >
+      <div className="volume-slider-bg">
+        <div className="volume-slider" ref={sliderRef}>
+          <div className="volume-slider-fill" style={{ height: `${isMuted ? 0 : volume * 100}%` }} />
+          <div className="volume-slider-thumb" style={{ bottom: `${Math.max(isMuted ? 0 : volume * 100 - 1, 0)}%` }} />
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/icons.tsx
@@ -0,0 +1,151 @@
+import { CSSProperties } from 'react';
+
+export const PlayIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24" width="20" height="20">
+    <path d="M20.494,7.968l-9.54-7A5,5,0,0,0,3,5V19a5,5,0,0,0,7.957,4.031l9.54-7a5,5,0,0,0,0-8.064Zm-1.184,6.45-9.54,7A3,3,0,0,1,5,19V5A2.948,2.948,0,0,1,6.641,2.328,3.018,3.018,0,0,1,8.006,2a2.97,2.97,0,0,1,1.764.589l9.54,7a3,3,0,0,1,0,4.836Z" />
+  </svg>
+);
+
+export const PauseIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24" width="20" height="20">
+    <path d="M6.5,0A3.5,3.5,0,0,0,3,3.5v17a3.5,3.5,0,0,0,7,0V3.5A3.5,3.5,0,0,0,6.5,0ZM8,20.5a1.5,1.5,0,0,1-3,0V3.5a1.5,1.5,0,0,1,3,0Z" />
+    <path d="M17.5,0A3.5,3.5,0,0,0,14,3.5v17a3.5,3.5,0,0,0,7,0V3.5A3.5,3.5,0,0,0,17.5,0ZM19,20.5a1.5,1.5,0,0,1-3,0V3.5a1.5,1.5,0,0,1,3,0Z" />
+  </svg>
+);
+
+export const FullscreenIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24" width="20" height="20">
+    <path d="M19,24H17a1,1,0,0,1,0-2h2a3,3,0,0,0,3-3V17a1,1,0,0,1,2,0v2A5.006,5.006,0,0,1,19,24Z" />
+    <path d="M1,8A1,1,0,0,1,0,7V5A5.006,5.006,0,0,1,5,0H7A1,1,0,0,1,7,2H5A3,3,0,0,0,2,5V7A1,1,0,0,1,1,8Z" />
+    <path d="M7,24H5a5.006,5.006,0,0,1-5-5V17a1,1,0,0,1,2,0v2a3,3,0,0,0,3,3H7a1,1,0,0,1,0,2Z" />
+    <path d="M23,8a1,1,0,0,1-1-1V5a3,3,0,0,0-3-3H17a1,1,0,0,1,0-2h2a5.006,5.006,0,0,1,5,5V7A1,1,0,0,1,23,8Z" />
+  </svg>
+);
+
+export const VolumeIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24" width="20" height="20">
+    <path d="M20.807,4.29a1,1,0,0,0-1.415,1.415,8.913,8.913,0,0,1,0,12.59,1,1,0,0,0,1.415,1.415A10.916,10.916,0,0,0,20.807,4.29Z" />
+    <path d="M18.1,7.291A1,1,0,0,0,16.68,8.706a4.662,4.662,0,0,1,0,6.588A1,1,0,0,0,18.1,16.709,6.666,6.666,0,0,0,18.1,7.291Z" />
+    <path d="M13.82.2A12.054,12.054,0,0,0,6.266,5H5a5.008,5.008,0,0,0-5,5v4a5.008,5.008,0,0,0,5,5H6.266A12.059,12.059,0,0,0,13.82,23.8a.917.917,0,0,0,.181.017,1,1,0,0,0,1-1V1.186A1,1,0,0,0,13.82.2ZM13,21.535a10.083,10.083,0,0,1-5.371-4.08A1,1,0,0,0,6.792,17H5a3,3,0,0,1-3-3V10A3,3,0,0,1,5,7h1.8a1,1,0,0,0,.837-.453A10.079,10.079,0,0,1,13,2.465Z" />
+  </svg>
+);
+
+export const NoVolumeIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 24 24" width="20" height="20">
+    <path d="m15.004,20.004v2.812c0,.297-.132.579-.36.769-.181.151-.407.231-.64.231-.061,0-.121-.005-.181-.017-2.999-.551-5.752-2.299-7.554-4.794h-1.27C2.242,19.004-.002,16.761-.002,14.002v-4.001C-.002,8.885.359,7.828,1.043,6.944c.34-.437.966-.516,1.403-.179.437.338.517.966.179,1.403-.41.529-.627,1.163-.627,1.832v4.001c0,1.655,1.347,3.002,3.002,3.002h1.78c.321,0,.622.154.811.414l.3.415c1.263,1.749,3.083,3.057,5.113,3.703v-1.532c0-.552.447-1,1-1s1,.448,1,1Zm8.703,3.703c-.195.195-.451.293-.707.293s-.512-.098-.707-.293L.293,1.707C-.098,1.316-.098.684.293.293S1.316-.098,1.707.293l4.623,4.623C8.131,2.468,10.855.753,13.822.205c.293-.053.594.025.821.215.229.19.36.472.36.769v12.402l1.686,1.686c.846-.887,1.31-2.046,1.31-3.276,0-1.269-.494-2.461-1.392-3.359-.391-.39-.391-1.023,0-1.414s1.023-.391,1.414,0c1.275,1.275,1.978,2.97,1.978,4.773,0,1.764-.673,3.425-1.896,4.69l1.415,1.415c3.33-3.418,3.304-8.908-.081-12.292-.391-.391-.391-1.023,0-1.414.391-.391,1.023-.391,1.414,0,4.164,4.164,4.191,10.922.081,15.12l2.774,2.774c.391.391.391,1.023,0,1.414ZM7.762,6.348l5.242,5.242V2.468c-2.032.647-3.854,1.955-5.114,3.702l-.128.178Z" />
+  </svg>
+);
+
+export const VolumeUpIcon = () => (
+  <svg width="234" height="216" viewBox="0 0 234 216" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g id="volume-bar-path">
+      <circle className="volume-bar-knob" cx="117" cy="110.323" r="59" stroke="white" stroke-width="10" />
+      <path
+        id="volume-bar-path-path"
+        d="M108.5 164.823V118.323C108.5 114.181 111.858 110.823 116 110.823H118C122.142 110.823 125.5 114.181 125.5 118.323L125.5 164.823H108.5Z"
+        fill="white"
+        stroke="white"
+      />
+    </g>
+    <circle
+      className="volume-bar-circle"
+      style={{ '--position': 1 } as CSSProperties}
+      cx="61.084"
+      cy="197.19"
+      r="12.5"
+      transform="rotate(123.919 61.084 197.19)"
+      fill="white"
+      stroke="white"
+    />
+    <circle
+      className="volume-bar-circle"
+      style={{ '--position': 2 } as CSSProperties}
+      cx="25.3066"
+      cy="154.372"
+      r="12.5"
+      transform="rotate(123.919 25.3066 154.372)"
+      fill="white"
+      stroke="white"
+    />
+    <circle
+      className="volume-bar-circle"
+      style={{ '--position': 3 } as CSSProperties}
+      cx="18.042"
+      cy="99.0481"
+      r="12.5"
+      transform="rotate(123.919 18.042 99.0481)"
+      fill="white"
+      stroke="white"
+    />
+    <circle
+      className="volume-bar-circle"
+      style={{ '--position': 4 } as CSSProperties}
+      cx="41.5522"
+      cy="48.4446"
+      r="12.5"
+      transform="rotate(123.919 41.5522 48.4446)"
+      fill="white"
+      stroke="white"
+    />
+    <circle
+      className="volume-bar-circle"
+      style={{ '--position': 5 } as CSSProperties}
+      cx="88.5171"
+      cy="18.3162"
+      r="12.5"
+      transform="rotate(123.919 88.5171 18.3162)"
+      fill="white"
+      stroke="white"
+    />
+    <circle
+      className="volume-bar-circle"
+      style={{ '--position': 6 } as CSSProperties}
+      cx="144.314"
+      cy="18.0422"
+      r="12.5"
+      transform="rotate(123.919 144.314 18.0422)"
+      fill="white"
+      stroke="white"
+    />
+    <circle
+      className="volume-bar-circle"
+      style={{ '--position': 7 } as CSSProperties}
+      cx="191.573"
+      cy="47.7092"
+      r="12.5"
+      transform="rotate(123.919 191.573 47.7092)"
+      fill="white"
+      stroke="white"
+    />
+    <circle
+      className="volume-bar-circle"
+      style={{ '--position': 8 } as CSSProperties}
+      cx="215.578"
+      cy="98.0803"
+      r="12.5"
+      transform="rotate(123.919 215.578 98.0803)"
+      fill="white"
+      stroke="white"
+    />
+    <circle
+      className="volume-bar-circle"
+      style={{ '--position': 9 } as CSSProperties}
+      cx="208.855"
+      cy="153.472"
+      r="12.5"
+      transform="rotate(123.919 208.855 153.472)"
+      fill="white"
+      stroke="white"
+    />
+    <circle
+      className="volume-bar-circle"
+      style={{ '--position': 10 } as CSSProperties}
+      cx="173.499"
+      cy="196.639"
+      r="12.5"
+      transform="rotate(123.919 173.499 196.639)"
+      fill="white"
+      stroke="white"
+    />
+  </svg>
+);
